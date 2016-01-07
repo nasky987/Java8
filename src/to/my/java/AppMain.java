99209e1cc5b900a59b09ff56e2f2e9f24357c8ca
JOHN LEE <nasky987@gmail.com>
2016-01-07 23:39:11 +0900

    2016. 01. 07 실습2 OOP 관련 실습인듯 List를 상속받아 몇개를 add했는지를 구하는 기능을 추가한 MyList 코드 작성예제

--- src/to/my/java/AppMain.java
+++ src/to/my/java/AppMain.java
@@ -3,15 +3,22 @@ package to.my.java;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * Created by Jhon on 2016-01-07.
  */
 public class AppMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (final FileReader fileReader = new FileReader(new File("D:\\Developer Data/InteliJ_Workspace/Java8/src/to/my/java/temp/test.txt"));
             final BufferedReader bufferedReader = new BufferedReader(fileReader)) {
 
@@ -31,6 +38,16 @@ public class AppMain {
             System.out.println(uniqueWords);
         }catch (Exception e) {
             e.printStackTrace();
+        }
+
+        try(final Stream<String> lines = Files.lines(Paths.get("D:\\Developer Data/InteliJ_Workspace/Java8/src/to/my/java/temp/test.txt"))){
+            System.out.println(
+                lines.map(line -> line.split("[\\s]+"))
+                        .flatMap(Arrays::stream)
+                        .distinct()
+                        .sorted()
+                        .collect(toList())
+            );
         }
     }
 }
---
+++ src/to/my/java/OopExample.java
@@ -0,0 +1,42 @@
+package to.my.java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Created by Jhon on 2016-01-07.
+ */
+public class OopExample {
+    public static void main(String[] args) {
+        MyList<Integer> list = new MyList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println("count: " + list.getCount());
+
+        MyList<Integer> list2 = new MyList<>();
+        list2.addAll(Arrays.asList(1, 2, 3, 4, 5));
+        System.out.println("count: " + list2.getCount());
+    }
+}
+
+class MyList<E> extends ArrayList<E> {
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public boolean addAll(final Collection c) {
+        count += c.size();
+        return super.addAll(c);
+    }
+
+    @Override
+    public boolean add(final E o) {
+        count++;
+        return super.add(o);
+    }
+}
