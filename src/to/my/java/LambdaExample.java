f163e6343f2cf70dd7f13b5fe8077e21b5bd7057
JOHN LEE <nasky987@gmail.com>
2016-01-08 11:56:49 +0900

    2016. 01. 08 실습3 람다 표현식 실습 우선 기존 사용하는 방식으로 10개의 숫자중 2보다 큰 수를 받는 리스트와 7보다 작은 리스트를 만드는 일반적인 코드를 작성

---
+++ src/to/my/java/LambdaExample.java
@@ -0,0 +1,28 @@
+package to.my.java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Jhon on 2016-01-08.
+ */
+public class LambdaExample {
+    public  static  void main(String[] args) {
+        final List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8 , 9, 10);
+        final List<Integer> result1 = new ArrayList<>();
+
+        for(Integer number : list) {
+            if(number > 2) {
+                result1.add(number);
+            }
+        }
+
+        final List<Integer> result2 = new ArrayList<>();
+        for(Integer number : list) {
+            if(number < 7 ) {
+                result2.add(number);
+            }
+        }
+    }
+}
