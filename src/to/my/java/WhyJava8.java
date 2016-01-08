b145c0ff7fbc825d67049768a7f8c5328a14f276
JOHN LEE <nasky987@gmail.com>
2016-01-08 15:36:31 +0900

    2016. 01. 08 실습 1 kevin님의 동영상 강좌 1회 1부터 10까지의 숫자 사이에 : 문자 추가하는 일반적인 코드 작성 for문으로 하니 복잡해지니 다시 for each로 회귀하여 마지막 글자를 삭제하는 방향으로 코드 수정 java에서 start, end를 받는 메서드의 경우 일반적으로 start는 포함(inclusive), end는 불포함(exclusive)이다. 이럴 때 항상 API문서를 참고하도록 하자.

--- src/to/my/java/WhyJava8.java
+++ src/to/my/java/WhyJava8.java
@@ -10,15 +10,13 @@ public class WhyJava8 {
     public static void main(String[] args) {
         final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        StringBuilder stringBuilder = new StringBuilder();
+        final StringBuilder stringBuilder = new StringBuilder();
+        for (Integer number : numbers) {
+            stringBuilder.append(number).append(" : ");
+        }
 
-        final int size = numbers.size();
-        for(int i=0; i<size; i++) {
-            stringBuilder.append(numbers.get(i));
-
-            if(i != size - 1) {
-                stringBuilder.append(":");
-            }
+        if(stringBuilder.length() > 0) {
+            stringBuilder.delete(stringBuilder.length() - 3, stringBuilder.length());
         }
 
         System.out.println(stringBuilder.toString());
