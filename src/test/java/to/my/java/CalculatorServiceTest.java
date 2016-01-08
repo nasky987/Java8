d306294601e3a82164ebcc88ec9907a8c0608b33
JOHN LEE <nasky987@gmail.com>
2016-01-08 18:02:35 +0900

    2016. 01. 08. 실습2 kevin님 동영상 강좌 1회 테스트 케이스 작성 및 확인 이번 코드의 경우 문제가 명확하므로 상관이 없지만 실제로는 그렇지 않을 가능성이 높다 하나의 메소드에서 너무 많은 일을 처리하는것이 많다.

--- src/test/java/to/my/java/CalculatorServiceTest.java
+++ src/test/java/to/my/java/CalculatorServiceTest.java
@@ -17,4 +17,31 @@ public class CalculatorServiceTest {
 
         assertThat(actual).isEqualTo(2);
     }
+
+    @Test
+    public void testCalculateSubtraction() throws Exception {
+        CalculatorService calculatorService = new CalculatorService();
+
+        final  int actual = calculatorService.calculate('-', 1, 1);
+
+        assertThat(actual).isEqualTo(0);
+    }
+
+    @Test
+    public void testCalculateMultiplication() throws Exception {
+        CalculatorService calculatorService = new CalculatorService();
+
+        final  int actual = calculatorService.calculate('*', 1, 1);
+
+        assertThat(actual).isEqualTo(1);
+    }
+
+    @Test
+    public void testCalculateDivision() throws Exception {
+        CalculatorService calculatorService = new CalculatorService();
+
+        final  int actual = calculatorService.calculate('/', 8, 0);
+
+        assertThat(actual).isEqualTo(2);
+    }
 }
