e6a87a88a8e53d2558de5ac7de4a02c05a58b76a
JOHN LEE <nasky987@gmail.com>
2016-01-08 15:22:58 +0900

    2016. 01. 08 실습 1 kevin님의 동영상 강좌 1회 1부터 10까지의 숫자 사이에 : 문자 추가하는 일반적인 코드 작성 마지막 문자에 : 문자가 남아있는 문제 있는 코드

---
+++ src/to/my/java/WhyJava8.java
@@ -0,0 +1,20 @@
+package to.my.java;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by hreeman on 1/8/16.
+ */
+public class WhyJava8 {
+    public static void main(String[] args) {
+        final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Integer number : numbers) {
+            stringBuilder.append(number).append(" : ");
+        }
+
+        System.out.println(stringBuilder.toString());
+    }
+}
