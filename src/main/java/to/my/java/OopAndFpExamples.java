63e8b84b1ad1e5f81fd5cb616cad99d732371454
JOHN LEE <nasky987@gmail.com>
2016-01-08 18:45:24 +0900

    2016. 01. 08. 실습2 kevin님 동영상 강좌 1회 이번 코드는 OOP의 오브젝트를 넘겨서 사용하는 방법에 대한 코드이다. 이와 같은 패턴의 처리를 Strategy Pattern이라고 한다.

--- src/main/java/to/my/java/OopAndFpExamples.java
+++ src/main/java/to/my/java/OopAndFpExamples.java
@@ -3,23 +3,29 @@ package to.my.java;
 /**
  * Created by hreeman on 1/8/16.
  */
-public class OopAnotherExample {
+public class OopAndFpExamples {
     public static void main(String[] args) {
         final CalculatorService calculatorService = new CalculatorService(new Addition(), new Subtraction());
-        final int additionResult = calculatorService.calculate(1, 1);
+        final int additionResult = calculatorService.calculate(11, 4);
         System.out.println(additionResult);
 
         final CalculatorService calculatorService2 = new CalculatorService(new Subtraction(), new Multiplication());
-        final int subtractionResult = calculatorService2.calculate(1, 1);
+        final int subtractionResult = calculatorService2.calculate(11, 1);
         System.out.println(subtractionResult);
 
         final CalculatorService calculatorService3 = new CalculatorService(new Multiplication(), new Divistion());
-        final int multiplicationResult = calculatorService3.calculate(1, 1);
+        final int multiplicationResult = calculatorService3.calculate(11, 2);
         System.out.println(multiplicationResult);
 
         final CalculatorService calculatorService4 = new CalculatorService(new Divistion(), new Addition());
-        final int divisionResult = calculatorService4.calculate(8, 4);
+        final int divisionResult = calculatorService4.calculate(20, 4);
         System.out.println(divisionResult);
+
+        final FpCalculatorService fpCalculatorService = new FpCalculatorService();
+        System.out.println("      addition: " + fpCalculatorService.calculate(new Addition(), 11, 4));
+        System.out.println("   subtraction: " + fpCalculatorService.calculate(new Subtraction(), 11, 1));
+        System.out.println("multiplication: " + fpCalculatorService.calculate(new Multiplication(), 11, 2));
+        System.out.println("      division: " + fpCalculatorService.calculate(new Divistion(), 20, 4));
     }
 }
 
@@ -74,6 +80,16 @@ class CalculatorService {
     public int compute(int num1, int num2) {
         if(num1 > 10 && num2 < num1) {
             return calculation2.calculate(num1, num2);
+        } else {
+            throw new IllegalArgumentException("Invalid input num1:" + num1 + ", num2: " + num2);
+        }
+    }
+}
+
+class FpCalculatorService {
+    public int calculate(Calculation calculation, int num1, int num2) {
+        if(num1 > 10 && num2 < num1) {
+            return calculation.calculate(num1, num2);
         } else {
             throw new IllegalArgumentException("Invalid input num1:" + num1 + ", num2: " + num2);
         }
