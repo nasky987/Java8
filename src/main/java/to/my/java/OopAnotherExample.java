b53ef4d29b54c117eb175c4d0dee3ae52dab6780
JOHN LEE <nasky987@gmail.com>
2016-01-08 18:21:05 +0900

    2016. 01. 08. 실습2 kevin님 동영상 강좌 1회 OOP의 특성을 이용하여 디자인 패턴이 적용된 코드 작성 모든 코드를 검사할 것이 아니라 하나의 경우에만 검사하면 된다.

--- src/main/java/to/my/java/OopAnotherExample.java
+++ src/main/java/to/my/java/OopAnotherExample.java
@@ -5,33 +5,63 @@ package to.my.java;
  */
 public class OopAnotherExample {
     public static void main(String[] args) {
-        final CalculatorService calculatorService = new CalculatorService();
-        final int additionResult = calculatorService.calculate('+', 1, 1);
+        final CalculatorService calculatorService = new CalculatorService(new Addition());
+        final int additionResult = calculatorService.calculate(1, 1);
         System.out.println(additionResult);
 
-        final int subtractionResult = calculatorService.calculate('-', 1, 1);
+        final CalculatorService calculatorService2 = new CalculatorService(new Subtraction());
+        final int subtractionResult = calculatorService2.calculate(1, 1);
         System.out.println(subtractionResult);
 
-        final int multiplicationResult = calculatorService.calculate('*', 1, 1);
+        final CalculatorService calculatorService3 = new CalculatorService(new Multiplication());
+        final int multiplicationResult = calculatorService3.calculate(1, 1);
         System.out.println(multiplicationResult);
 
-        final int divisionResult = calculatorService.calculate('/', 8, 4);
+        final CalculatorService calculatorService4 = new CalculatorService(new Divistion());
+        final int divisionResult = calculatorService4.calculate(8, 4);
         System.out.println(divisionResult);
     }
 }
 
+interface Calculation {
+    int calculate(int num1, int num2);
+}
+
+class Addition implements Calculation {
+    @Override
+    public int calculate(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+
+class Subtraction implements  Calculation {
+    @Override
+    public int calculate(int num1, int num2) {
+        return num1 - num2;
+    }
+}
+
+class Multiplication implements Calculation {
+    @Override
+    public int calculate(int num1, int num2) {
+        return num1 * num2;
+    }
+}
+
+class Divistion implements Calculation {
+    @Override
+    public int calculate(int num1, int num2) {
+        return num1 / num2;
+    }
+}
 class CalculatorService {
-    public int calculate(char calculation, int num1, int num2) {
-        if (calculation == '+') {
-            return num1 + num2;
-        } else if (calculation == '-') {
-            return num1 - num2;
-        } else if(calculation == '*') {
-            return num1 * num2;
-        } else if (calculation == '/') {
-            return num1 / num2;
-        }else {
-            throw new IllegalArgumentException("Unknown calculation: " + calculation);
-        }
+    private final Calculation calculation;
+
+    public CalculatorService(Calculation calculation) {
+        this.calculation = calculation;
+    }
+
+    public int calculate(int num1, int num2) {
+        return calculation.calculate(num1, num2);
     }
 }
