5f1eb3b11d7ce940b34b5a7b7b70a9a12fdaec37
JOHN LEE <nasky987@gmail.com>
2016-01-08 15:40:38 +0900

    2016. 01. 08 실습 1 kevin님의 동영상 강좌 1회 중간에 구분자가 변경되면 문제가 생기니 구분자의 길이를 구하는곳에서 사용하도록 변경하고, 또 그렇게 되면 하나 수정하면 다른곳도 수정해야하니 하나의 문자열로 만들어서 사용을 하게 변경하게 될 것이다.

--- src/to/my/java/WhyJava8.java
+++ src/to/my/java/WhyJava8.java
@@ -11,12 +11,14 @@ public class WhyJava8 {
         final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         final StringBuilder stringBuilder = new StringBuilder();
+        final String separator = " : - ";
+
         for (Integer number : numbers) {
-            stringBuilder.append(number).append(" : ");
+            stringBuilder.append(number).append(separator);
         }
 
         if(stringBuilder.length() > 0) {
-            stringBuilder.delete(stringBuilder.length() - 3, stringBuilder.length());
+            stringBuilder.delete(stringBuilder.length() - separator.length(), stringBuilder.length());
         }
 
         System.out.println(stringBuilder.toString());
