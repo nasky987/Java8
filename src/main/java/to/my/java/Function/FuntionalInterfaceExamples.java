8b1c3d1755062d7ee06da3480c27d6e6688beb05
JOHN LEE <nasky987@gmail.com>
2016-01-13 11:09:39 +0900

    2016. 01. 13. 실습6 kevin님 동영상 강좌 5회 Custom Functional Interface 코드 작성 하나의 베이스 인터페이스를 제네릭을 이용하여 다양하게 사용하는 방법을 보여주는 예제 코드 작성

--- src/main/java/to/my/java/Function/FuntionalInterfaceExamples.java
+++ src/main/java/to/my/java/Function/FuntionalInterfaceExamples.java
@@ -105,6 +105,9 @@ public class FuntionalInterfaceExamples {
 
     public static void main(String[] args) {
         println(1, 2, 3, (i1, i2, i3) -> String.valueOf(i1 + i2 + i3));
+        println("Area is ", 12, 20, (message, length, width) -> message + (length * width));
+        println(1L, "John", "test@email.com",
+                (id, name, email) -> "User info: ID=" + id + ", name=" + name + ", email=" + email);
     }
 
     private static <T1, T2, T3> void println(T1 t1, T2 t2, T3 t3, Function3<T1, T2, T3, String> function) {
