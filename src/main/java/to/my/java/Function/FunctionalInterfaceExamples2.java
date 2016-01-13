bba5c17b37c3387ab6fa96abf718a9c1f15dc30c
JOHN LEE <nasky987@gmail.com>
2016-01-13 14:50:52 +0900

    2016. 01. 13. 실습9 kevin님 동영상 강좌 7회 필터 메서드를 작성하는데, 함수형 인터페이스인 Predicate를 이용하여 조건에 맞으면 것만 출력할 수 있도록 코드 수정

--- src/main/java/to/my/java/Function/FunctionalInterfaceExamples2.java
+++ src/main/java/to/my/java/Function/FunctionalInterfaceExamples2.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * Created by hreeman on 1/13/16.
@@ -17,18 +18,27 @@ public class FunctionalInterfaceExamples2 {
                 new Product(1L, "A", new BigDecimal("10.00")),
                 new Product(2L, "B", new BigDecimal("55.00")),
                 new Product(3L, "C", new BigDecimal("17.45")),
-                new Product(4L, "D", new BigDecimal("23.00")),
+                new Product(4L, "D", new BigDecimal("20.00")),
                 new Product(5L, "E", new BigDecimal("110.99"))
         );
         System.out.println(products);
 
-        List<Product> result = new ArrayList<>();
-        for(final Product product : products) {
-            if(product.getPrice().compareTo(new BigDecimal("20")) >= 0) {
-                result.add(product);
+        final BigDecimal twenty = new BigDecimal("20");
+        final List<Product> result = filter(products, product -> product.getPrice().compareTo(twenty) >= 0);
+        System.out.println("products >= $20:" + result);
+        System.out.println("products <= $10:" +
+                filter(products, product -> product.getPrice().compareTo(new BigDecimal("10")) <= 0));
+    }
+
+    private static <T> List<T> filter(List<T> list, Predicate<T> predicate) {
+        final List<T> result = new ArrayList<>();
+        for(final T t : list) {
+            if(predicate.test(t)) {
+                result.add(t);
             }
         }
-        System.out.println(result);
+
+        return result;
     }
 }
 
