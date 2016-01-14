fcb67b2d0a038bb6656e9fd311054903de320a3e
JOHN LEE <nasky987@gmail.com>
2016-01-14 17:52:04 +0900

    2016. 01. 14. 실습14 kevin님 동영상 강좌 8회 StreamAPI 알아보기 Price가 30이상인 값을 출력. 이 때, 리스트로 결과를 받지 않고 스트링으로 받아 개행을 시켜주면 좀더 보기 편하게 될 것이다.

---
+++ src/main/java/to/my/java/Function/StreamExaples4.java
@@ -0,0 +1,64 @@
+package to.my.java.Function;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Created by hreeman on 1/14/16.
+ */
+public class StreamExaples4 {
+    public static void main(String[] args) {
+        final List<Product> products =
+                Arrays.asList(
+                        new Product(1L, "A", new BigDecimal("100.50")),
+                        new Product(2L, "B", new BigDecimal("23.00")),
+                        new Product(3L, "C", new BigDecimal("31.45")),
+                        new Product(4L, "D", new BigDecimal("80.20")),
+                        new Product(5L, "E", new BigDecimal("7.50"))
+                );
+
+        System.out.println("Products.getPrice() >= 30 " +
+            products.stream()
+                    .filter(product -> product.getPrice().compareTo(new BigDecimal("30")) >= 0)
+                    .collect(toList())
+        );
+
+        System.out.println("Products.getPrice() >= 30(with joining): \n" +
+            products.stream()
+                    .filter(product -> product.getPrice().compareTo(new BigDecimal("30")) >= 0)
+                    .map(product -> product.toString())
+                    .collect(joining("\n"))
+        );
+    }
+}
+
+@AllArgsConstructor
+@Data
+class Product {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+}
+
+@AllArgsConstructor
+@Data
+class OrderedItem {
+    private Long id;
+    private Product product;
+    private int quantity;
+}
+
+@AllArgsConstructor
+@Data
+class Order {
+    private Long id;
+    private List<OrderedItem> items;
+}
