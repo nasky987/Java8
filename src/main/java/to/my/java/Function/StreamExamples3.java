8226ec51667066e9bedcbd61c1f35105f4ac299a
JOHN LEE <nasky987@gmail.com>
2016-01-14 16:40:41 +0900

    2016. 01. 14. 실습13 kevin님 동영상 강좌 8회 StreamAPI 알아보기 Stream API의 메소드들을 간단히 알아본다. collect메소드는 모와주는 역할이라고 보면되는데, 스트링(문자의 배열 즉 문자열이므로 집합으로 볼 수 있다.)이나, List, Set등으로 반환을 할 수 있다. collect메소드에 인자값으로 무엇을 넣는냐에 따라 달라지며 toList()는 리스트, toSet()은 셋, Joining()은 문자열이다.  이때, joining()은 구분자, 접두사, 접미사 등을 넣어서 문자열을 만들 수 있다.

---
+++ src/main/java/to/my/java/Function/StreamExamples3.java
@@ -0,0 +1,56 @@
+package to.my.java.Function;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
+
+/**
+ * Created by hreeman on 1/14/16.
+ */
+public class StreamExamples3 {
+    public static void main(String[] args) {
+        System.out.println("collection(toList()): " +
+            Stream.of(1, 3, 3, 5, 5)
+                    .filter(i -> i > 2)
+                    .map(i -> i * 2)
+                    .map(i -> "#" + i)
+                    .collect(toList())
+        );
+
+        System.out.println("collection(toSet()): " +
+                Stream.of(1, 3, 3, 5, 5)
+                        .filter(i -> i > 2)
+                        .map(i -> i * 2)
+                        .map(i -> "#" + i)
+                        .collect(toSet())
+        );
+
+        System.out.println("collection(joining()): " +
+                Stream.of(1, 3, 3, 5, 5)
+                        .filter(i -> i > 2)
+                        .map(i -> i * 2)
+                        .map(i -> "#" + i)
+                        .collect(joining())
+        );
+
+        System.out.println("collection(joining(\" , \")): " +
+                Stream.of(1, 3, 3, 5, 5)
+                        .filter(i -> i > 2)
+                        .map(i -> i * 2)
+                        .map(i -> "#" + i)
+                        .collect(joining(", "))
+        );
+
+        System.out.println("collection(joining(\" , \", \"[\", \"]\")): " +
+                Stream.of(1, 3, 3, 5, 5)
+                        .filter(i -> i > 2)
+                        .map(i -> i * 2)
+                        .map(i -> "#" + i)
+                        .collect(joining(", ", "[", "]"))
+        );
+    }
+}
