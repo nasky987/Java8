79435692cf508b5232e2d86f2b3a95c89a2d0f18
JOHN LEE <nasky987@gmail.com>
2016-01-14 21:30:20 +0900

    2016. 01. 14. 실습15 kevin님 동영상 강좌 8회 Stream을 이용한 Parallel Programming 병렬프로그래밍은 순서대로 처리하지 않음을 확인 할 수 있다. 또한 요소당 1초씩 걸리는 처리에 대해서 quad-core에서는 8개의 요소를 2초에 처리하지만 9개의 요소의 경우 총 3초가 걸린것을 확인할 수 있다. 즉, 1초에 4개코어에서 1개씩 처리하므로 1초에 4개 2초에 8개 이므로 9개일 경우 그 다음에 처리가 되므로 총 3초가 걸리는것을 알 수 있다.

--- src/main/java/to/my/java/Function/StreamExaples5Parallel.java
+++ src/main/java/to/my/java/Function/StreamExaples5Parallel.java
@@ -1,6 +1,9 @@
 package to.my.java.Function;
 
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Created by hreeman on 1/14/16.
@@ -11,13 +14,72 @@ public class StreamExaples5Parallel {
         IntStream.range(0, 100)
                 .forEach(i -> sum[0] += i);
 
-        System.out.println("sum: " + sum[0]);
+        System.out.println("                   sum (with side-effect): " + sum[0]);
 
         final int[] sum2 = { 0 };
         IntStream.range(0, 100)
                 .parallel()
                 .forEach(i -> sum2[0] += i);
 
-        System.out.println("parallel sum: " + sum2[0]);
+        System.out.println("          parallel sum (with side-effect): " + sum2[0]);
+
+        System.out.println("         stream sum (without side-effect): " +
+            IntStream.range(0, 100)
+                    .sum()
+        );
+
+        System.out.println("parallel stream sum (without side-effect): " +
+            IntStream.range(0, 100)
+                    .parallel()
+                    .sum()
+        );
+
+        System.out.println();
+        System.out.println("stream");
+        final long start = System.currentTimeMillis();
+        Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)
+                .stream()
+                .map(i -> {
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return i;
+                })
+                .forEach(i -> System.out.println(i));
+        System.out.println(System.currentTimeMillis() - start);
+
+        System.out.println();
+        System.out.println("parallel stream (4core 8elements)");
+        final long start2 = System.currentTimeMillis();
+        Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)
+                .parallelStream()
+                .map(i -> {
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return i;
+                })
+                .forEach(i -> System.out.println(i));
+        System.out.println(System.currentTimeMillis() - start2);
+
+        System.out.println();
+        System.out.println("parallel stream (4core 9elements)");
+        final long start3 = System.currentTimeMillis();
+        Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9)
+                .parallelStream()
+                .map(i -> {
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return i;
+                })
+                .forEach(i -> System.out.println(i));
+        System.out.println(System.currentTimeMillis() - start3);
     }
 }
