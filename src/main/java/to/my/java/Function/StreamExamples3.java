d0b4651234573f2e176fa724ce797340dc5f79fd
JOHN LEE <nasky987@gmail.com>
2016-01-14 17:02:52 +0900

    2016. 01. 14. 실습13 kevin님 동영상 강좌 8회 StreamAPI 알아보기 Integer3과 Primitive(원시)타입의 3이 오토박싱이 되어 다른 오브젝트여서 메모리상에 위치가 달라 찾지 못해야하는데, 찾는 이유와 오토박싱의 캐싱에 대한 설명 자바에서는 Integer타입의 경우 미리 -128~127까지 캐싱을 해놓는다. 그렇기 때문에 -128~127사이의 숫자는 새로운 오브젝트를 생성하지 않고 캐싱된 오브젝트를 재사용한다. 하지만 128이후부터는 미리 만들어진 오브젝트가 없으므로 새로 생성하게 되는데, 이 때 부터 오브젝트간의 메모리가 다른곳에 존재하므로 찾지 못하는 결과를 보여준다.

--- src/main/java/to/my/java/Function/StreamExamples3.java
+++ src/main/java/to/my/java/Function/StreamExamples3.java
@@ -70,5 +70,32 @@ public class StreamExamples3 {
                         .distinct()
                         .collect(toList())
         );
+
+        System.out.println(
+                Stream.of(1, 2, 3, 4, 5)
+                    .filter(i -> i == 3)
+                    .findFirst()
+        );
+
+        final Integer integer3 = 3;
+        System.out.println(
+                Stream.of(1, 2, 3, 4, 5)
+                    .filter(i -> i == integer3)
+                    .findFirst()
+        );
+
+        final Integer integer127 = 127;
+        System.out.println(
+                Stream.of(1, 2, 3, 4, 5, 127)
+                    .filter(i -> i == integer127)
+                    .findFirst()
+        );
+
+        final Integer integer128 = 128;
+        System.out.println(
+                Stream.of(1, 2, 3, 4, 5, 128)
+                    .filter(i -> i == integer128)
+                    .findFirst()
+        );
     }
 }
