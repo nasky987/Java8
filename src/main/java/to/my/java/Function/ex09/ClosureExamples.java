e3709e6a26fbe8bcc13ad6590347defc226498c1
JOHN LEE <nasky987@gmail.com>
2016-01-15 15:55:54 +0900

    2016. 01. 15. 실습18 kevin님 동영상 강좌 9회 Closure의 개념 설명 및 간단 예제 코드 작성 람다 표현식은 자체 scope가 지정되지 않기 때문에 익명클래스에 this와는 다르게 클래스 인스턴스 변수에 매칭되는것을 확인할 수 있다. 예제에서는 toString()메소드 호출하는 것을 통해 같은 방식의 코드이고 같은 this인데 다른곳을 가르키는 것을 확인 할 수 있다.

--- src/main/java/to/my/java/Function/ex09/ClosureExamples.java
+++ src/main/java/to/my/java/Function/ex09/ClosureExamples.java
@@ -6,9 +6,34 @@ package to.my.java.Function.ex09;
 public class ClosureExamples {
     private int number = 999;
 
+    @Override
+    public String toString() {
+        return new StringBuilder("ClosureExamples{")
+                .append("number=")
+                .append(number)
+                .append('}')
+                .toString()
+                ;
+    }
 
     public static void main(String[] args) {
         new ClosureExamples().test1();
+        new ClosureExamples().test2();
+    }
+
+    private void test2() {
+        final int number = 100;
+
+        testClosure("Anonymous Class",
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println(this.toString());
+                    }
+                }
+        );
+
+        testClosure("Lambda Expression", () -> System.out.println(this.toString()));
     }
 
     private void test1() {
@@ -23,7 +48,7 @@ public class ClosureExamples {
                 }
         );
 
-        testClosure("Lambda Expression", () -> System.out.println(number));
+        testClosure("Lambda Expression", () -> System.out.println(this.number));
     }
 
     private static void testClosure(final String name, Runnable runnable) {
