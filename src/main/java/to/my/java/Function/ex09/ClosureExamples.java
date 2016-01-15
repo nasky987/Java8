d1c026b4e7c9644e22adb351a802d0bf8beebedc
JOHN LEE <nasky987@gmail.com>
2016-01-15 14:48:11 +0900

    2016. 01. 15. 실습18 kevin님 동영상 강좌 9회 Closure를 사용하던 방식 재현 코드 작성 자바 스윙에서 이벤트 리스너등에서 사용하던 것처럼 코드 작성. final의 함정 설명 익명 클래스에서 외부 변수 접근시 반드시 외부 변수는 final로 선언되어야했지만, java8부터는 final 키워드가 없어도 접근은 가능해졌다. 하지만, 변경을 하면 그때 오류가 발생한다.

--- src/main/java/to/my/java/Function/ex09/ClosureExamples.java
+++ src/main/java/to/my/java/Function/ex09/ClosureExamples.java
@@ -7,23 +7,20 @@ public class ClosureExamples {
     public static void main(String[] args) {
         final int number = 100;
 
-        System.out.println("Anonymous Class");
+        testClosure("Anonymous Class",
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println(number);
+                    }
+                }
+        );
 
-        final Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println(number);
-            }
-        };
-        runnable.run();
-
-        System.out.println("Lambda");
-        final Runnable runnable2 = () -> System.out.println(number);
-        runnable2.run();
+        testClosure("Lambda Expression", () -> System.out.println(number));
     }
 
     private static void testClosure(final String name, Runnable runnable) {
-        System.out.println("Start " + name + ": ");
+        System.out.println(name + ": ");
         runnable.run();
     }
 }
