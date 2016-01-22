01c809c18ad3320b50958c48249e69a0c2dc9816
JOHN LEE <nasky987@gmail.com>
2016-01-22 21:48:40 +0900

    2016. 01. 22. 실습22 kevin님 동영상 강좌 11회 Method Reference 사용 예제 작성 Product를 상속받는 ProductA, ProductB가 동일한 생성을 할 때, 개별적으로 만드는것이 아니라 제네릭과 조합하여 Product를 상속받는 어떤 타입 T가 들어올 것임을 명시해놓는다. 만약 ProductC가 나왔어도 그대로 사용하면 될 것이다.

--- src/main/java/to/my/java/Function/ex11/MethodReferenceExample2Constructor.java
+++ src/main/java/to/my/java/Function/ex11/MethodReferenceExample2Constructor.java
@@ -24,17 +24,49 @@ public class MethodReferenceExample2Constructor {
         System.out.println(section1WithMethodReference);
 
         System.out.println("================================================");
-        final Product product = new Product(1L, "A", new BigDecimal("100"));
-        System.out.println(product);
+        final OldProduct oldProduct = new OldProduct(1L, "A", new BigDecimal("100"));
+        System.out.println(oldProduct);
 
-        final ProductCreator productCreator = Product::new;
-        System.out.println(productCreator.create(1L, "A", new BigDecimal("100")));
+        final OldProductCreator OldProductCreator = OldProduct::new;
+        System.out.println(OldProductCreator.create(1L, "A", new BigDecimal("100")));
+
+        System.out.println("================================================");
+
+        final ProductA a = createProduct(1L, "A", new BigDecimal("123"), ProductA::new);
+        final ProductB b = createProduct(1L, "A", new BigDecimal("123"), ProductB::new);
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+    private static <T extends Product> T createProduct(final Long id,
+                                                final String name,
+                                                final BigDecimal price,
+                                                final ProductCreator<T> productCreator) {
+        if(id == null || id < 1L) {
+            throw new IllegalArgumentException("The id must be a positive Long.");
+        }
+
+        if(name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("The name is not given.");
+        }
+
+        if(price == null || BigDecimal.ZERO.compareTo(price) >= 0) {
+            throw new IllegalArgumentException("The price is must be greater then 0.");
+        }
+
+        return productCreator.create(id, name, price);
     }
 }
 
 @FunctionalInterface
-interface ProductCreator {
-    Product create(Long id, String name, BigDecimal price);
+interface ProductCreator <T extends Product> {
+    T create(Long id, String name, BigDecimal price);
+}
+
+@FunctionalInterface
+interface OldProductCreator {
+    OldProduct create(Long id, String name, BigDecimal price);
 }
 
 @AllArgsConstructor
@@ -45,8 +77,38 @@ class Section {
 
 @AllArgsConstructor
 @Data
-class Product {
+abstract class Product {
     private Long id;
     private String name;
     private BigDecimal price;
+}
+
+@AllArgsConstructor
+@Data
+class OldProduct {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+}
+
+class ProductA extends Product {
+    public ProductA(Long id, String name, BigDecimal price) {
+        super(id, name, price);
+    }
+
+    @Override
+    public String toString() {
+        return "A=" + super.toString();
+    }
+}
+
+class ProductB extends Product {
+    public ProductB(Long id, String name, BigDecimal price) {
+        super(id, name, price);
+    }
+
+    @Override
+    public String toString() {
+        return "B=" + super.toString();
+    }
 }
